cf435af8635aacb456216db77adee7ada525f3db
Toma Bogdan <bogdantoma2000@gmail.com>
2024-10-16 11:59:52 +0300

    add - initial setup

---
+++ controllers/appController.js
@@ -0,0 +1,3 @@
+exports.displayIndex = (req, res) => {
+  res.render("index");
+};
---
+++ router/appRouter.js
@@ -0,0 +1,5 @@
+const { Router } = require("express");
+const appController = require("../controllers/appController");
+const router = Router();
+
+router.get("/", appController.displayIndex);
